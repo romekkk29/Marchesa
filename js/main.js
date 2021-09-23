3f1cc0775acf666daab449f6df72b5d6c112ebfb
romekkk29 <romegomez29@gmail.com>
2021-09-23 12:17:52 -0300

    primer subida

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const btnMenu= document.querySelector(".menu");
+const menu= document.querySelector(".menu-bar")
+btnMenu.addEventListener("click",function(){
+    menu.classList.toggle("menu-bar2")
+})

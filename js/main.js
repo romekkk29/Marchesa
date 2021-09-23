18a59819bc73a9af73c211aea990fd6287c26aa5
romekkk29 <romegomez29@gmail.com>
2021-09-23 18:47:51 -0300

    probando

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
-const btnMenu= document.querySelector(".menu");
+  const btnMenu= document.querySelector(".menu2");
 const menu= document.querySelector(".menu-bar")
 btnMenu.addEventListener("click",function(){
-    menu.classList.toggle("menu-bar2")
-})
+    menu.classList.toggle("isactive")
+})  
+/*  function menuBar (){
+    document.querySelector(".menu-bar").classList.toggle("menu-bar2");
+}
+document.querySelector(".menu").onclick = function(){
+    menuBar();
+};  */

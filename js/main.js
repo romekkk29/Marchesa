07a8b3fddd7ee59fa8c00f4e752fb3bc68bdcd4e
romekkk29 <romegomez29@gmail.com>
2021-09-24 15:10:41 -0300

    scrilll

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
   const btnMenu= document.querySelector(".menu");
-const menu= document.querySelector(".menu-bar")
+const menu= document.querySelector(".menu-bar");
 btnMenu.addEventListener("click",function(){
-    menu.classList.toggle("isactive")
+    window.onscroll= function(){window.scrollTo(window.scrollX,window.scrollY)};
+    menu.classList.toggle("isactive");
+    
 })  
 /*  function menuBar (){
     document.querySelector(".menu-bar").classList.toggle("menu-bar2");
@@ -9,3 +11,26 @@ btnMenu.addEventListener("click",function(){
 document.querySelector(".menu").onclick = function(){
     menuBar();
 };  */
+/* let scrolll= [];
+window.addEventListener("scroll",function(){
+    
+for (let i=0; i<window.scrollY;i++){
+    scrolll.push(window.scrollY);
+}
+console.log(scrolll);
+})
+function scrollear(){
+    window.scrollTo({
+      top: scrolll[scrolll.length-10],
+    })
+    
+} */
+/* function disableScroll(){  
+    var x = window.scrollX;
+    var y = window.scrollY;
+    window.onscroll = function(){ window.scrollTo(x, y) };
+}
+function enableScroll(){  
+    window.onscroll = null;
+}
+ */

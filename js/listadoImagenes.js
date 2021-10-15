2e3f0f24bd90ed38a73b4acb3285136108e2b73a
romekkk29 <romegomez29@gmail.com>
2021-10-15 13:05:50 -0300

    ss

--- js/listadoImagenes.js
+++ js/listadoImagenes.js
@@ -3,7 +3,7 @@ let listadoImagenes=
         title: "POSITANO",
         precio: "$4000",
         description: "esta re buena",
-        imgUrl:["img/2/positano.jpg","img/2/positano5.jpg","img/2/prueba2.jpg"]
+        imgUrl:["img/2/positano.jpg","img/2/positano5.jpg"]
 
     };
     

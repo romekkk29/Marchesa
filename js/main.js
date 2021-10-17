7326731696c9324bc04dc82fbf74398fa136180a
romekkk29 <romegomez29@gmail.com>
2021-10-17 02:03:55 -0300

    fotos de mierda

--- js/main.js
+++ js/main.js
@@ -1,41 +1,12 @@
   const btnMenu= document.querySelector(".menu");
 const menu= document.querySelector(".menu-bar");
 btnMenu.addEventListener("click",function(e){
-    //window.onscroll= function(){window.scrollTo(window.scrollX,window.scrollY)};
     e.preventDefault();
     menu.classList.toggle("isactive");
     
 })  
-/*  function menuBar (){
-    document.querySelector(".menu-bar").classList.toggle("menu-bar2");
-}
-document.querySelector(".menu").onclick = function(){
-    menuBar();
-};  */
-/* let scrolll= [];
-window.addEventListener("scroll",function(){
-    
-for (let i=0; i<window.scrollY;i++){
-    scrolll.push(window.scrollY);
-}
-console.log(scrolll);
-})
-function scrollear(){
-    window.scrollTo({
-      top: scrolll[scrolll.length-10],
-    })
-    
-} */
-/* function disableScroll(){  
-    var x = window.scrollX;
-    var y = window.scrollY;
-    window.onscroll = function(){ window.scrollTo(x, y) };
-}
-function enableScroll(){  
-    window.onscroll = null;
-}
- */
-var imagenes2= ["img/2/ABRU.jpg","img/2/candy.jpg","img/2/find2.jpg","img/2/KYLIE.jpg","img/2/lele.jpg"]
+
+var imagenes2= ["../img/3/gales.jpeg","../img/3/juanatacoalto.jpeg","../img/3/ojota.jpeg","../img/3/tacobajo.jpeg"]
 
 let cont=0;
 function carrousel(contenedor,imagenes){
@@ -119,76 +90,3 @@ function escribirHTML(objeto) {
 }
 
 
-/*  const slider = document.querySelector(".rome2");
-let slides= Array.from(document.querySelectorAll(".preRome"));
-
-let isDragging= false;
-let startPos=0;
-let currentTranslate=0;
-let prevTranslate=0;
-let animationID=0;
-let currentIndex=0;
-
-slides.forEach((slide,index) =>{
-    const slideImg=slide ;
-    slideImg.addEventListener("dragstart", e => e.preventDefault())
-    slide.addEventListener("touchstart", touchStart(index))
-    slide.addEventListener("touchend", touchEnd)
-    slide.addEventListener("touchmove", touchMove)
-
-
-    slide.addEventListener("mousedown", touchStart(index))
-    slide.addEventListener("mouseup", touchEnd)
-    slide.addEventListener("mouseleave", touchEnd)
-    slide.addEventListener("mousemove", touchMove)
-})
-window.oncontextmenu= function(event){
-    event.preventDefault()
-    event.stopPropagation()
-    return false 
-}
-function touchStart(index){
-    return function(event){
-        currentIndex= index;
-        startPos =getPositionX(event)
-        isDragging=true;
-        animationID= requestAnimationFrame;
-        slider.classList.add("grabbing")
-    }
-}
-function touchEnd(){
-    isDragging=false;
-    cancelAnimationFrame(animationID);
-    const movedBy=currentTranslate-prevTranslate;
-    if (movedBy<-25 && currentIndex<slides.length-1)
-    currentIndex +=1
-    if (movedBy>25  && currentIndex>0)
-    currentIndex -=1
-    setPositionByIndex()
-    slider.classList.remove("grabbing");
-
-}
-function touchMove(event){
-    if(isDragging){
-            const currentPosition= getPositionX(event);
-            currentTranslate=prevTranslate + currentPosition- startPos
-    }
-}
-function getPositionX(event){
-    return event.type.includes("mouse") ? event.pageX : event.touches[0].clientX ;
-}
-
-function animation(){
-      setSliderPosition()
-        
-        if(isDragging) requestAnimationFrame(animation)
-}
-function  setSliderPosition(){
-    slider.style.transform = `translateX(${currentTranslate}px)`
-}
-function setPositionByIndex(){
-    currentTranslate= currentIndex * -window.innerWidth
-    prevTranslate= currentTranslate
-    setSliderPosition()
-}
-  */

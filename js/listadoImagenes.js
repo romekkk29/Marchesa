0cfa149f4b74c75eba611be65672f87a60ace23b
romekkk29 <romegomez29@gmail.com>
2021-10-17 12:36:07 -0300

    soy genio

--- js/listadoImagenes.js
+++ js/listadoImagenes.js
@@ -4,6 +4,24 @@ let listadoImagenes=[
         precio: "$4000",
         description: "Hablemos de ese Borcego de Cuero comodín para todos los días! El Borcego Laika es de cuero liso con puño caña mediana y plataforma tap rock intermedio de goma eva y mide 6 cm.",
         imgUrl:["img/2/positano.jpg","img/2/positano5.jpg"]
-
-    }];
+    },
+    {
+        title: "GALES",
+        precio: "$4000",
+        description: "Hablemos de ese Borcego de Cuero comodín para todos los días! El Borcego Laika es de cuero liso con puño caña mediana y plataforma tap rock intermedio de goma eva y mide 6 cm.",
+        imgUrl:["img/3/gales.jpeg","img/3/gales.jpeg"]
+    },    
+    {
+        title: "TACOALTO",
+        precio: "$4000",
+        description: "Hablemos de ese Borcego de Cuero comodín para todos los días! El Borcego Laika es de cuero liso con puño caña mediana y plataforma tap rock intermedio de goma eva y mide 6 cm.",
+        imgUrl:["img/3/juanatacoalto.jpeg","img/3/juanatacoalto.jpeg"]
+    },
+    {
+        title: "OJOTA",
+        precio: "$4000",
+        description: "Hablemos de ese Borcego de Cuero comodín para todos los días! El Borcego Laika es de cuero liso con puño caña mediana y plataforma tap rock intermedio de goma eva y mide 6 cm.",
+        imgUrl:["img/3/ojota.jpeg","img/3/ojota.jpeg"]
+    },
+];
     
--- js/main.js
+++ js/main.js
@@ -6,29 +6,36 @@ btnMenu.addEventListener("click",function(e){
     
 })  
 
-var imagenes2= ["img/3/gales.jpeg","img/3/juanatacoalto.jpeg","img/3/ojota.jpeg","img/3/tacobajo.jpeg"]
-
+var imagenes2= ["img/3/gales.jpeg","img/3/juanatacoalto.jpeg","img/3/ojota.jpeg","img/3/tacobajo.jpeg"];
+var class2=["gales","tacoalto","ojota","tacobajo"];
 let cont=0;
 function carrousel(contenedor,imagenes){
     contenedor.addEventListener("click", e => {
         let atras=contenedor.querySelector(".atras");
         let adelante= contenedor.querySelector(".adelante");
         let imagen=contenedor.querySelector("img");
+      
         let tgt=e.target;
+        e.preventDefault();
         if(tgt==atras){
             if(cont >0){
-                imagen.src= imagenes[cont -1];
+                imagen.src= imagenes[cont -1 ];
+               
                 cont--;
             }else { imagen.src= imagenes[imagenes.length - 1];
                      cont= imagenes.length - 1;
             }
+            console.log(cont);
         } else if(tgt==adelante) {
             if(cont < imagenes.length-1){
-                imagen.src= imagenes[cont +1];
+                imagen.src= imagenes[cont + 1 ];
+               
                 cont++;
             }else { imagen.src= imagenes[0];
+
                      cont= 0;
             }
+            console.log(cont);
         }
     })
 }
@@ -38,6 +45,31 @@ document.addEventListener("DOMContentLoaded", ()=>{
 })
 
 let imgSrc=[];
+  let zxyzz=document.querySelector(".stokeame");
+  zxyzz.addEventListener("click",myFunction2);
+function myFunction2(){
+    if(cont==1){
+        escribirHTML(listadoImagenes[1]);
+        let cancelar=document.querySelector(".teQuiero");
+         cancelar.style.display="none";
+         let contenedor3=document.querySelector(".norte");
+        carrousel(contenedor3,imgSrc) ;
+        let carrito=document.querySelector(".carrito");
+        carrito.style.display="block";
+        carrito.style.display="flex";
+        carrito.style.alignItems="center";
+        carrito.style.justifyContent="center";
+        let este=document.querySelector(".este");
+        este.style.display="none";
+        let imagennn=document.querySelector("#stockk");
+        imagennn.style.width="260px";
+        imagennn.style.height="260px";
+         }
+    
+   } 
+
+    
+
  let p=document.querySelector(".ssss");
   p.addEventListener("click",myFunction);
 function myFunction(){
@@ -53,9 +85,8 @@ carrito.style.alignItems="center";
 carrito.style.justifyContent="center";
 let este=document.querySelector(".este");
 este.style.display="none";
-
-    }
-
+ }
+  
    
 function escribirHTML(objeto) {
     for(let imagen of objeto.imgUrl){
@@ -72,7 +103,7 @@ function escribirHTML(objeto) {
     <div class="adelante botones">
         &#62
     </div>
-    <img src=${imgSrc[0]}>
+    <img id=stockk src=${imgSrc[0]}>
     </div>
     
     <p>${objeto.title}</p>

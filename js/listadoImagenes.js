a6856746510145d48fa30e0032e8fa5c4c8208c3
romekkk29 <romegomez29@gmail.com>
2021-10-28 21:40:50 -0300

    cambiando precios

--- js/listadoImagenes.js
+++ js/listadoImagenes.js
@@ -2,398 +2,398 @@
 let listadoImagenes=[
     {
         title: "SUNNY",
-        precio: "$3100",
+        precio: "$3500",
         description: "Sandalias de goma inyectada  | Plataforma de 4.5cm | Color nude oscuro | Numero 39/40",
         imgUrl:["img/SKYLA/principal.jpeg",]
     },
  
     {
         title: "GALES",
-        precio: "$4500 Ef",
+        precio: "$5000 Ef",
         description: "Precio $6200 - 25% OFF | Botas caña alta 100% cuero | Numero 37 sin reingreso",
         imgUrl:["img/3/gales.jpeg","img/3/gales.jpeg"]
     },    
     {
         title: "GIN",
-        precio: "$2800",
+        precio: "$4000",
         description: "Sandalias cuero | Taco 15cm | Color negro | Numero 37 sin reingreso",
         imgUrl:["img/3/juanatacoalto.jpeg","img/3/juanatacoalto.jpeg"]
     },
     {
         title: "MALI WHITE",
-        precio: "$2300",
+        precio: "$3400",
         description: "Capellada de cuero | Base de goma eva | Plataforma 2cm | Color blanco | Numero 38 sin reingreso",
         imgUrl:["img/3/ojota.jpeg","img/3/ojota.jpeg"]
     },
     {
         title: "UMA BLACK",
-        precio: "$2800",
+        precio: "$3800",
         description: "Capellada de cuero | Taco 6cm | Color negro | Numero 37 sin reingreso.",
         imgUrl:["img/3/tacobajo.jpeg","img/3/tacobajo.jpeg"]
     },
     {
         title: "ASIC",
-        precio: "$4900",
+        precio: "$5100",
         description: "Sandalias de cuero | color negro | Plataforma: 2.5cm",
         imgUrl:["img/ASICCUERO_/principal.jpg","img/ASICCUERO_/1.jpg","img/ASICCUERO_/2.jpg","img/ASICCUERO_/3.jpg"]
     },
     {
         title: "BERRY",
-        precio: "$5500",
+        precio: "$5800",
         description: "Sandalias de cuero vacuno con apliques | Base de goma eva de 8cm | Color negro",
         imgUrl:["img/BERRYCUERO_/principal3.jpg","img/BERRYCUERO_/1.jpeg","img/BERRYCUERO_/2.jpg"]
     },
     {
         title: "BLOSAM",
-        precio: "$5100",
+        precio: "$5500",
         description: "Sandalias de cuero | Base de goma eva de 8cm | Color negro & blanco",
         imgUrl:["img/BLOSAM/principal.jpg","img/BLOSAM/1.jpg","img/BLOSAM/2.jpg","img/BLOSAM/3.jpg"]
     },
     {
         title: "SHIRA",
-        precio: "$3900",
+        precio: "$4300",
         description: "Sandalias de cuero con tachas | Base de 6cm | Color negro",
         imgUrl:["img/SHIRA/principal.jpg","img/SHIRA/1.jpg","img/SHIRA/2.jpeg"]
     },
     {
         title: "CLOE",
-        precio: "$4900",
+        precio: "$5100",
         description: "Sandalias de cuero con apliques en color plateado | Plataforma de 5cm Taco 6.5cm | Color negro",
         imgUrl:["img/CLOE/principal1.jpg","img/CLOE/2.jpg","img/CLOE/3.jpg"]
     },
     {
         title: "FINNI",
-        precio: "$4600",
+        precio: "$4900",
         description: "Sandalias de cuero | Base de goma eva de 4cm | Color negro blanco print ",
         imgUrl:["img/FINNI/4.JPG","img/FINNI/principal1.jpg","img/FINNI/1.jpeg","img/FINNI/2.jpeg","img/FINNI/3.jpeg"]
     },
     {
         title: "FLAUNT",
-        precio: "$4600",
+        precio: "$4900",
         description: "Sandalias de cuero con detalle trenzado | Taco de 7cm | Color negro & nude ",
         imgUrl:["img/FLAUNT/principal1.jpg","img/FLAUNT/1.jpg","img/FLAUNT/2.jpg","img/FLAUNT/3.jpg"]
     },
     {
         title: "FORM",
-        precio: "$4600",
+        precio: "$4900",
         description: "Sandalias de cuero en colores pastel | Base de 2.5cm | Color negro nude lila rosa blanco ",
         imgUrl:["img/FORMYVEGAN/principal1.jpeg","img/FORMYVEGAN/1.jpg","img/FORMYVEGAN/2.jpg","img/FORMYVEGAN/3.jpeg","img/FORMYVEGAN/4.jpg","img/FORMYVEGAN/5.jpeg","img/FORMYVEGAN/6.jpeg","img/FORMYVEGAN/7.jpeg","img/FORMYVEGAN/8.jpeg"]
     },
     {
         title: "GALES",
-        precio: "$5100",
+        precio: "$5400",
         description: "Sandalias de cuero con detalles en plateado | Plataforma de 9cm | Color negro ",
         imgUrl:["img/GIULIA/principal.jpeg","img/GIULIA/1.jpg","img/GIULIA/2.jpeg","img/GIULIA/3.jpg"]
     },
     {
         title: "SUNNY",
-        precio: "$3100",
+        precio: "$3500",
         description: "Sandalias de goma inyectada  | Plataforma de 4.5cm | Color negro rosa nude naranja blanco ",
         imgUrl:["img/SKYLA/principal.jpeg","img/SKYLA/0.jpeg","img/SKYLA/1.png","img/SKYLA/2.png","img/SKYLA/3.jpg"]
     },
     {
         title: "GRETA",
-        precio: "$4600",
+        precio: "$4900",
         description: "Sandalias de cuero | Plataforma de goma eva de 8cm | Color negro & nude",
         imgUrl:["img/NANDA/principal1.jpg","img/NANDA/1.jpg","img/NANDA/2.JPG","img/NANDA/3.jpg","img/NANDA/4.jpg"]
     },
     {
         title: "KALEB",
-        precio: "$4700",
+        precio: "$5100",
         description: "Sandalias de cuero detalles en trenza | Plataforma de 2cm | Color negro blanco suela",
         imgUrl:["img/KALEB/principal.jpeg","img/KALEB/1.jpeg","img/KALEB/2.jpeg","img/KALEB/3.jpeg"]
     },
     {
         title: "WIPPEI",
-        precio: "$5300",
+        precio: "$5700",
         description: "Sandalias de cuero | Plataforma de 5cm Taco 7cm | Color negro & suela",
         imgUrl:["img/WIPEI/principal.jpeg" ,"img/WIPEI/1.jpeg" ,"img/WIPEI/2.jpg"]
     },
     {
         title: "SIUX",
-        precio: "$4900",
+        precio: "$5400",
         description: "Sandalias de cuero con apliques en plateado | Plataforma de 4cm | Color negro",
         imgUrl:["img/SIUX/principal.jpeg" ,"img/SIUX/1.jpeg" ,"img/SIUX/2.jpeg","img/SIUX/3.jpeg"]
     },
     {
         title: "PLEO",
-        precio: "$4800",
+        precio: "$5200",
         description: "Sandalias de cuero con apliques y hebillas | Color negro",
         imgUrl:["img/PLEO/principal.jpeg"]
     },
     {
         title: "DAFF",
-        precio: "$5100",
+        precio: "$5500",
         description: "Sandalias de cuero | Plataforma 5cm | Capellada zebra, combinado con color suela beige negro",
         imgUrl:["img/DAFF/principal.PNG","img/DAFF/1.jpeg","img/DAFF/2.jpeg","img/DAFF/3.jpg","img/DAFF/4.jpeg","img/DAFF/5.png"]
     },
     {
         title: "NIKKA",
-        precio: "$5300",
+        precio: "$5700",
         description: "Sandalias de cuero con apliques | Plataforma 9cm | Color negro",
         imgUrl:["img/NIKKA/principal.jpeg","img/NIKKA/1.jpg"]
     },
     {
         title: "MALI",
-        precio: "$5600",
+        precio: "$5800",
         description: "Sandalias de cuero con detalle en taco | Color negro & nude",
         imgUrl:["img/MALI/principal.jpeg","img/MALI/1.jpeg","img/MALI/2.jpeg","img/MALI/3.jpg","img/MALI/4.jpg"]
     },
     {
         title: "LAST",
-        precio: "$4900",
+        precio: "$5300",
         description: "Sandalias de cuero con tachas | Plataforma 6cm | Color negro",
         imgUrl:["img/LAST/principal.jpeg","img/LAST/1.jpeg","img/LAST/2.jpeg"]
     },
     {
         title: "KIMBER",
-        precio: "$4150",
+        precio: "$4550",
         description: "Sandalias de cuero | Taco 6cm | Color negro & nude",
         imgUrl:["img/KIMBERLY/principal.jpg","img/KIMBERLY/1.jpg","img/KIMBERLY/2.jpg","img/KIMBERLY/3.jpg","img/KIMBERLY/4.jpg"]
     },
     {
         title: "NOSSA",
-        precio: "$4900",
+        precio: "$5300",
         description: "Sandalias de cuero con capellada print | Plataforma 9cm | Color negro",
         imgUrl:["img/NOSSA/principal.jpeg"]
     },
     {
         title: "MONACO",
-        precio: "$4200",
+        precio: "$4700",
         description: "Sandalias de cuero | Taco 6cm | Color negro & nude",
         imgUrl:["img/TOKEN/principal.jpeg","img/TOKEN/1.jpeg","img/TOKEN/2.jpeg","img/TOKEN/3.jpeg","img/TOKEN/4.jpeg","img/TOKEN/5.jpeg","img/TOKEN/5.jpeg","img/TOKEN/6.jpeg","img/TOKEN/8.jpg"]
     },
     {
         title: "ATENAS",
-        precio: "$4900",
+        precio: "$5300",
         description: "Sandalias de cuero | Taco 5cm | Color negro & blanco",
         imgUrl:["img/ATENAS/principal.jpeg"]
     },
     {
         title: "COMFY",
-        precio: "$3500",
+        precio: "$3900",
         description: "Sandalias de cuero con apliques | Taco 6cm | Color negro & blanco",
         imgUrl:["img/COMFY/principal.jpeg","img/COMFY/2.jpeg"]
     },
     {
         title: "HELENA",
-        precio: "$4300",
+        precio: "$4700",
         description: "Sandalias de cuero | Plataforma 4cm | Color negro & dorado",
         imgUrl:["img/DREYA/principal.jpeg","img/DREYA/1.jpeg","img/DREYA/2.jpeg"]
     },
     {
         title: "CARMELA",
-        precio: "$4400",
+        precio: "$4700",
         description: "Sandalias de cuero | Taco 6cm | Color negro blanco rosa",
         imgUrl:["img/TURY/principal.jpeg","img/TURY/1.jpeg","img/TURY/2.jpeg","img/TURY/3.jpeg","img/TURY/4.jpeg","img/TURY/5.jpg","img/TURY/6.jpg"]
     },
     {
         title: "TOKIO",
-        precio: "$4400",
+        precio: "$4700",
         description: "Sandalias de cuero con apliques en cuero | Plataforma 4cm | Color negro",
         imgUrl:["img/RICH/principal.jpeg","img/RICH/1.jpeg"]
     },
     {
         title: "PAXXY",
-        precio: "$5100",
+        precio: "$5600",
         description: "Sandalias de cuero con apliques | Plataforma 9cm | Color negro",
         imgUrl:[]
     },
     {
         title: "GRETA",
-        precio: "$3600",
+        precio: "$3900",
         description: "Capellada de cuero | Plataforma 4cm | Color negro & nude",
         imgUrl:["img/RENI/principal.jpeg","img/RENI/1.jpeg","img/RENI/2.jpeg","img/RENI/3.jpg"]
     },
     {
         title: "NEST",
-        precio: "$3700",
+        precio: "$3900",
         description: "Capellada de cuero | Base 6cm | Color negro",
         imgUrl:["img/NEST/principal.jpeg","img/NEST/1.jpg","img/NEST/2.jpg","img/NEST/3.jpeg"]
     },
     {
         title: "CLARI",
-        precio: "$4400",
+        precio: "$4700",
         description: "Sandalias de cuero brillantes | Color black & gold",
         imgUrl:["img/MASOL/PRINCIPAL.jpeg","img/MASOL/2DA.jpeg","img/MASOL/3RA.jpeg","img/MASOL/4TA.jpeg"]
     },
     {
         title: "CRETA",
-        precio: "$3700",
+        precio: "$3900",
         description: "Sandalias de cuero con tachas y hebilla | Plataforma 3cm | Color black camel nude white ",
         imgUrl:["img/sn3/1.jpeg","img/sn3/3.jpeg"]
     },
     {
         title: "EVIAL",
-        precio: "$4100",
+        precio: "$4400",
         description: "Capellada de cuero detalle con trenza | Taco 6cm | Color negro",
         imgUrl:["img/EVIAL/principal.jpeg","img/EVIAL/1.jpeg","img/EVIAL/2.jpeg","img/EVIAL/3.jpg"]
     },
     {
         title: "DRUNA",
-        precio: "$5500",
+        precio: "$5700",
         description: "Capellada de cuero con apliques en plateado | Plataforma 4cm | Color negro camel blanco",
         imgUrl:["img/DRUNA/principal.jpeg","img/DRUNA/1.jpeg"]
     },
     {
         title: "BRUSEL",
-        precio: "$4900",
+        precio: "$5300",
         description: "Sandalias de cuero con tiras de rafia | Base de goma eva 5cm | Color camel",
         imgUrl:["img/BRUSEL/principal.jpeg","img/BRUSEL/1.jpg","img/BRUSEL/2.jpg"]
     },
     {
         title: "CLEO",
-        precio: "$3800",
+        precio: "$4100",
         description: "Sandalias de cuero con tachas y hebilla | Base de goma eva 2cm | Color camel negro rosa lila ",
         imgUrl:["img/sn2/4.jpeg","img/sn2/1.jpeg","img/sn2/2.jpeg","img/sn2/3.jpeg","img/sn2/5.jpeg"]
     },
     {
         title: "ABRU",
-        precio: "$3900",
+        precio: "$4100",
         description: "Zapas vegan | Color negro blanco rosa",
         imgUrl:["img/ABRU/principal.PNG","img/ABRU/1.jpg","img/ABRU/2.png","img/ABRU/3.png","img/ABRU/4.png"]
     },
     {
         title: "CANDY",
-        precio: "4100",
+        precio: "4400",
         description: "Zapas de cuero y lona estanpada" ,
         imgUrl:["img/CANDY/principal.JPG","img/CANDY/1.jpg"]
     },
     {
         title: "CEBRAL",
-        precio: "$4100",
+        precio: "$4400",
         description: "Zapas de cuero y lona estanpada cebra" ,
         imgUrl:["img/CEBRAL/principal.PNG","img/CEBRAL/1.PNG","img/CEBRAL/2.PNG"]
     },
     {
         title: "CRANT",
-        precio: "$5100",
+        precio: "$5400",
         description: "Zapas de cuero y lona estanpada animal print" ,
         imgUrl:["img/CRANT/prinpal1.png","img/CRANT/principal.png","img/CRANT/1.png"]
     },
     {
         title: "TRUCK",
-        precio: "$4100",
+        precio: "$4500",
         description: "Zapas vegan",
         imgUrl:["img/ERAZ/principal.PNG","img/ERAZ/1.PNG","img/ERAZ/2.PNG"]
     },
     {
         title: "FIND",
-        precio: "$3900",
+        precio: "$4300",
         description: "Zapas vegan | Color gold & white",
         imgUrl:["img/FIND/principal.JPG","img/FIND/1.jpg","img/FIND/2.jpg"]
     },
     {
         title: "KYLIE",
-        precio: "$3500",
+        precio: "$3800",
         description: "Zapas vegan incluye cadena y chapita | Color rainbow print white",
         imgUrl:["img/KYLIE/principal.PNG","img/KYLIE/1.jpeg","img/KYLIE/2.jpeg","img/KYLIE/3.jpg","img/KYLIE/4.JPG"]
     },
     {
         title: "LELE",
-        precio: "$3800",
+        precio: "$4100",
         description: "Zapas de cuero | Color blanco & combinado",
         imgUrl:["img/LELE/principal.JPG","img/LELE/1.jpeg"]
     },
     {
         title: "MARIV",
-        precio: "$3800",
+        precio: "$4100",
         description: "Zapas vegan con abrojos | Color lila rosa verde fucsia",
         imgUrl:["img/MARIV/principal.jpeg","img/MARIV/1.jpeg","img/MARIV/2.jpeg","img/MARIV/3.jpg","img/MARIV/4.jpeg"]
     },
     {
         title: "TRONIC",
-        precio: "$4900",
+        precio: "$5100",
         description: "Zapas de cuero | Color gold & white",
         imgUrl:["img/troni/principal.jpeg" ,"img/troni/1.jpg","img/troni/2.jpg","img/troni/3.jpg"]
     },
     {
         title: "LUBA",
-        precio: "$3500",
+        precio: "$3800",
         description: "Sandalias de cuero | Color negro lila rosa nude amarillo",
         imgUrl:["img/LUBA/1.jpg","img/LUBA/2.jpg","img/LUBA/3.jpg"]
     },
     {
         title: "MALIBU",
-        precio: "$3000",
+        precio: "$3500",
         description: "Super tendencia para este verano| Sandalias de goma inyectada | Color negro chocolate naranja rosado verde pastel",
         imgUrl:["img/malibu/2.jpeg","img/malibu/1.jpeg","img/malibu/3.jpeg"]
     },
     {
         title: "BATTLO",
-        precio: "$3500",
+        precio: "$3800",
         description: "Botinetas de cuero con cierre interno | Color negro lila blanco",
         imgUrl:["img/BATTLO/principal1.jpg" ,"img/BATTLO/1.jpeg","img/BATTLO/2.jpg","img/BATTLO/3.jpg"  ]
     },
     {
         title: "JUDY",
-        precio: "$5500",
+        precio: "$5900",
         description: "Tex de cuero sin cierre | Taco 6cm | Color celeste rosa blanco lila",
         imgUrl:["img/JUDY/principal.jpeg" ,"img/JUDY/1.jpeg","img/JUDY/2.jpeg","img/JUDY/3.jpeg","img/JUDY/4.jpeg","img/JUDY/5.jpg","img/JUDY/6.jpg"]
     },
     {
         title: "YUKY",
-        precio: "$5800",
+        precio: "$5900",
         description: "Tex de cuero bordadas | Color blanco negro suela",
         imgUrl:["img/YUKY/principal.JPG" ,"img/YUKY/1.jpg","img/YUKY/2.jpeg","img/YUKY/3.jpeg","img/YUKY/4.jpg","img/YUKY/5.jpg"]
     },
     {
         title: "BELGICA",
-        precio: "$3600",
+        precio: "$3900",
         description: "Sandalias de cuero | Color animal print & cebra",
         imgUrl:["img/sinNombre/1.jpeg" ,"img/sinNombre/2.jpeg"]
     },
     {
         title: "HERMES",
-        precio: "$3900",
+        precio: "$4100",
         description: "Sandalias de cuero | Plataforma 5cm | Color negro camel rosa nude",
         imgUrl:["img/sn4/1.jpeg","img/sn4/2.jpeg"]
     },
     {
         title: "MARRUECOS",
-        precio: "3900",
+        precio: "4100",
         description: "Sandalias de cuero con detalles y abrojo | Plataforma 6cm | Color camel",
         imgUrl:["img/sn5/1.jpeg","img/sn5/2.jpeg"]
     },
     {
         title: "INDIAN",
-        precio: "$3800",
+        precio: "$4100",
         description: "Sandalias de cuero con tachas | Color cow & black",
         imgUrl:["img/sn6/2.jpeg","img/sn6/1.jpeg"]
     },
     {
         title: "VENECIA",
-        precio: "$3800",
+        precio: "$4100",
         description: "Sandalias de cuero con tachas | Plataforma 3cm | Color negro camel rosa",
         imgUrl:["img/sn7/1.jpeg","img/sn7/2.jpeg"]
     },
     {
         title: "MALU",
-        precio: "$3300",
+        precio: "$3700",
         description: "Sandalias de gamuza | Taco 5cm | Color negro arena rojo fucsia verde ",
         imgUrl:["img/sn8/5.jpeg","img/sn8/6.jpeg","img/sn8/7.jpeg"]
     },
     {
         title: "MER",
-        precio: "$3800",
+        precio: "$4100",
         description: "Sandalias de cuero con tachas | Color negro suela blanco",
         imgUrl:["img/sn9/1.jpeg","img/sn9/2.jpeg","img/sn9/3.jpeg"]
     },
     {
         title: "ROMA",
-        precio: "$4100",
+        precio: "$4500",
         description: "Sandalias de cuero con tachas y hebillas | Plataforma 6cm | Color negro & suela",
         imgUrl:["img/sn10/1.jpeg","img/sn10/2.jpeg"]
     },
     {
         title: "RUT",
-        precio: "$3300",
+        precio: "$3500",
         description: "Sandalias vegan de charol | Plataforma 1.5 Taco 6cm | Color negro nude gris celeste",
         imgUrl:["img/sn11/1.jpeg","img/sn11/2.jpeg","img/sn11/3.jpeg","img/sn11/4.jpeg","img/sn11/5.jpeg"]
     },
     {
         title: "IBIZA",
-        precio: "$3500",
+        precio: "$3700",
         description: "Sandalias de gamuza |  Color negro nude fucsia rojo",
         imgUrl:["img/sn12/1.jpeg","img/sn12/2.jpeg","img/sn12/3.jpeg","img/sn12/4.jpeg"]
     },
